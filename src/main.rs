98650047c37c89176e811375128dd96c9afa48aa
Jeff Smith <jffreyjs@gmail.com>
2019-12-04 22:55:02 -0500

    tcp boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::{
+    net::{TcpListener, ToSocketAddrs},
+    prelude::*,
+    task,
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+fn main() -> Result<()> {
+    let fut = accept_loop("127.0.0.1:8080");
+    println!("Listening on port 8080");
+    task::block_on(fut)
+}
+
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    let mut incoming = listener.incoming();
+    while let Some(_stream) = incoming.next().await {
+        // TODO
+    }
+    Ok(())
 }
