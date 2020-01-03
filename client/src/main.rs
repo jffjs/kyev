a7e2860609bf76ce6719ed57392df424a17de1b4
Jeff Smith <jsmith3@vonage.com>
2020-01-03 16:46:18 -0500

    move server to own package

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,40 @@
-use std::io::{prelude::*, stdin, stdout, BufRead, BufReader};
-use std::net::{TcpStream, ToSocketAddrs};
+use std::io::{prelude::*, stdin, stdout, BufReader};
+use std::net::TcpStream;
+
+extern crate clap;
+use clap::{App, Arg};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 fn main() -> Result<()> {
-    let stream = TcpStream::connect("127.0.0.1:8080").expect("Couldn't connect to server...");
+    let matches = App::new("kyev-cli")
+        .version("0.1.0")
+        .author("Jeff Smith")
+        .arg(
+            Arg::with_name("hostname")
+                .short("h")
+                .long("hostname")
+                .value_name("HOSTNAME")
+                .help("Host name of kyev server")
+                .default_value("127.0.0.1"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .value_name("PORT")
+                .help("Port of kyev server")
+                .default_value("8080"),
+        )
+        .get_matches();
+
+    let host = format!(
+        "{}:{}",
+        matches.value_of("hostname").unwrap_or_default(),
+        matches.value_of("port").unwrap_or_default()
+    );
+
+    let stream = TcpStream::connect(&host).expect("Couldn't connect to server...");
     let (reader, mut writer) = (&stream, &stream);
     let mut buf_reader = BufReader::new(reader);
 
@@ -12,7 +42,7 @@ fn main() -> Result<()> {
         let mut input = String::new();
         let mut output = String::new();
 
-        write_prompt();
+        write_prompt(&host);
         stdin().read_line(&mut input)?;
 
         writer.write(input.as_bytes())?;
@@ -21,7 +51,7 @@ fn main() -> Result<()> {
     }
 }
 
-fn write_prompt() {
-    print!("127.0.0.1:8080> ");
+fn write_prompt(host: &str) {
+    print!("{}> ", host);
     stdout().flush().unwrap();
 }
