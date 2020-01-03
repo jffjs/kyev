6eb9bc0ab09fca265315561ee29d44f28ec588c8
Jeff Smith <jsmith3@vonage.com>
2020-01-03 15:02:47 -0500

    echo client

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,14 +4,20 @@ use std::net::{TcpStream, ToSocketAddrs};
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 fn main() -> Result<()> {
-    // let stream = TcpStream::connect("127.0.0.1:8080");
-    // let (reader, mut writer) = (&stream, &stream);
+    let stream = TcpStream::connect("127.0.0.1:8080").expect("Couldn't connect to server...");
+    let (reader, mut writer) = (&stream, &stream);
+    let mut buf_reader = BufReader::new(reader);
 
     loop {
-        write_prompt();
         let mut input = String::new();
+        let mut output = String::new();
+
+        write_prompt();
         stdin().read_line(&mut input)?;
-        print!("{}", input);
+
+        writer.write(input.as_bytes())?;
+        buf_reader.read_line(&mut output)?;
+        print!("{}", output);
     }
 }
 
